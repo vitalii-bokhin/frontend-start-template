94f95ecf0a80fe005d14feba12fdf9014028135a
Lufter <lufter21@gmail.com>
2017-10-09 20:41:47 +0300

    modified send.php

--- send.php
+++ send.php
@@ -1,6 +1,7 @@
 <?php
 $mail="lufter21@gmail.com";
 $utext = '<strong>Телефон:</strong> '.$_POST['tel'].'<br />';
+$utext .= '<strong>Сообщение:</strong> '.$_POST['message'].'<br />';
 $title="Заявка с сайта dealersair.com";
 $header="Content-type: text/html; charset=\"utf-8\"\r\n";
 $header.="From: dealersair.com <info@dealersair.com>\r\n";
