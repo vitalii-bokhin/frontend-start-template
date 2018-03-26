4d5e0e4ccf4e01cb16e7db575a08662b25425bf5
Lufter <lufter21@gmail.com>
2018-03-26 13:44:24 +0300

    fix

--- js/mouseparallax.js
+++ js/mouseparallax.js
@@ -1,50 +1,87 @@
-function Mouseparallax(elem, opt) {
-	var def = {
-		listener: window,
-		deltaX: 50,
-		deltaY: 50,
+function Mouseparallax(elem, options) {
+	var _ = this,
+	defaultOpt = {
+		listener: document,
+		deltaX: 21,
+		deltaY: 21,
+		rangeX: [],
+		rangeY: [],
 	},
-	opt = opt || {},
-	options = $.extend({}, def, opt),
-	animate = false,
+	options = options || {},
+	opt = $.extend({}, defaultOpt, options),
 	$elem = $(elem),
 	startMousePos = {X: 0, Y: 0},
-	listenerW = $(options.listener).innerWidth(),
-	listenerH = $(options.listener).innerHeight(),
-	startElementPos = {X: 0, Y: 0};
+	listenerW = $(opt.listener).innerWidth(),
+	listenerH = $(opt.listener).innerHeight();
 
+	_.translateElement = {X: 0, Y: 0};
+	_.startElementPos = {X: 0, Y: 0};
 
-	$(options.listener).on('mouseenter', function(e) {
+	if ($elem.attr('data-delta-x') != undefined) {
+		opt.deltaX = +$elem.attr('data-delta-x');
+	}
+
+	if ($elem.attr('data-delta-y') != undefined) {
+		opt.deltaY = +$elem.attr('data-delta-y');
+	}
+
+	if ($elem.attr('data-range-x') != undefined) {
+		opt.rangeX = $elem.attr('data-range-x').split(',');
+	}
+
+	if ($elem.attr('data-range-y') != undefined) {
+		opt.rangeY = $elem.attr('data-range-y').split(',');
+	}
+	
+	$(document).on('mouseenter', opt.listener, function(e) {
 		startMousePos.X = e.clientX;
 		startMousePos.Y = e.clientY;
 	});
 
-	$(options.listener).on('mouseleave', function(e) {
-		startElementPos.X = translateElement.X;
-		startElementPos.Y = translateElement.Y;
+	$(document).on('mouseleave', opt.listener, function(e) {
+		_.startElementPos.X = _.translateElement.X;
+		_.startElementPos.Y = _.translateElement.Y;
 	});
 
-	$(options.listener).on('mousemove', function(e) {
+	$(document).on('mousemove', opt.listener, function(e) {
+
 		var deltaMouse = {
 			X: e.clientX - startMousePos.X,
 			Y: e.clientY - startMousePos.Y
 		};
 
-		translateElement = {
-			X: deltaMouse.X * (options.deltaX / listenerW) + startElementPos.X,
-			Y: deltaMouse.Y * (options.deltaY / listenerH) + startElementPos.Y
+		_.translateElement = {
+			X: deltaMouse.X * (opt.deltaX / listenerW) + _.startElementPos.X,
+			Y: deltaMouse.Y * (opt.deltaY / listenerH) + _.startElementPos.Y
 		};
 
-console.log(translateElement);
+		var translateX = _.translateElement.X,
+		translateY = _.translateElement.Y;
 
-		$elem.css({transform: 'translate3d('+ translateElement.X +'px, '+ translateElement.Y +'px, 0px)'});
+		if (opt.rangeX || opt.rangeY) {
+
+			if (_.translateElement.X >= opt.rangeX[0]) {
+				translateX = opt.rangeX[0];
+			} else if (_.translateElement.X <= opt.rangeX[1]) {
+				translateX = opt.rangeX[1];
+			}
+
+			if (_.translateElement.Y >= opt.rangeY[0]) {
+				translateY = opt.rangeY[0];
+			} else if (_.translateElement.Y <= opt.rangeY[1]) {
+				translateY = opt.rangeY[1];
+			}
+			
+		}
+
+		$elem.css({transform: 'translate3d('+ translateX +'px, '+ translateY +'px, 0px)'});
 
 	});
 
 }
 
 var mousePlxObj = [], i = 0, ind;
-function mouseparallax(elem, opt) {
+function mouseparallax(elem, options) {
 	if ($(elem)[0].ind == undefined) {
 		$(elem)[0].ind = ind = i;
 	} else {
@@ -52,7 +89,7 @@ function mouseparallax(elem, opt) {
 	}
 
 	if (!(mousePlxObj[elem+ind] instanceof Mouseparallax)) {
-		mousePlxObj[elem+ind] = new Mouseparallax(elem, opt);
+		mousePlxObj[elem+ind] = new Mouseparallax(elem, options);
 	}
 
 	i++;
