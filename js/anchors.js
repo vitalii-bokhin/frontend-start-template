a1754afe903ab08ae066dd3878932929dc061986
Lufter <lufter21@gmail.com>
2018-04-01 21:33:15 +0300

    fix popup and anchor script

--- js/anchors.js
+++ js/anchors.js
@@ -13,5 +13,23 @@ $(document).ready(function() {
 
 		return false;
 	});
+
+	if (window.location.hash) {
+
+		var anch = window.location.hash;
+
+		if($(anch).length && !$(anch).hasClass('popup__window')){
+
+			$('html, body').stop().animate({scrollTop: 0}, 1);
+
+			window.onload = function() {
+				var scrTo = $(anch).offset().top - $('.header').innerHeight() - 21;
+				$('html, body').stop().animate({scrollTop: scrTo}, 1021, 'easeInOutQuart');
+			}
+
+		}
+
+	}
 	
 });
+
--- js/popup.js
+++ js/popup.js
@@ -177,22 +177,11 @@ $(document).ready(function() {
 	});
 
 
-
 	if (window.location.hash) {
-
-		var anch = window.location.hash;
-
-		if($(anch).length && !$(anch).hasClass('popup__window')){
-
-			$('html, body').stop().animate({scrollTop: 0}, 1);
-
-			window.onload = function() {
-				var scrTo = $(anch).offset().top - $('#js-top-header').innerHeight() - 21;
-				$('html, body').stop().animate({scrollTop: scrTo}, 1021, 'easeInOutQuart');
-			}
-
+		var hash = window.location.hash;
+		if($(hash).length && $(hash).hasClass('popup__window')){
+			Popup.show(hash);
 		}
-		
 	}
 
 });
