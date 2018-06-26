63bc8c201caeb099e3d6005dcae99b14efd5d194
Lufter <lufter21@gmail.com>
2018-06-27 01:27:00 +0300

    com

--- dist/js/script.js
+++ dist/js/script.js
@@ -105,6 +105,21 @@ Element.prototype.elementIsHidden = function() {
 }
 
 }());
+(function() {
+	"use strict";
+
+	//fix header
+	var headerElem = document.querySelector('.header');
+
+	document.addEventListener('scroll', function() {
+		if (window.pageYOffset > 21) {
+			headerElem.classList.add('header_fixed');
+		} else {
+			headerElem.classList.remove('header_fixed');
+		}
+	});
+
+}());
 /*
 * call Toggle.init(Str button selector[, Str toggle class]);
 */
