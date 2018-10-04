dde27f6300cf946c9ec422c7678120bb258a914d
Lufter <lufter21@gmail.com>
2018-10-04 05:02:44 +0300

    maskinput

--- src/modules/form/maskinput/maskinput.js
+++ src/modules/form/maskinput/maskinput.js
@@ -11,19 +11,50 @@ var Maskinput;
 		var defValue = '';
 
 		this.tel = function() {
-			if (!/^\+7?((?<=\d)\(\d{0,3})?((?<=[\d\(])\)\d{0,3})?((?<=\)[\d\-]*)(\-\d{0,2})){0,2}$/.test(inputElem.value)) {
+			if (!/^[\+\d\(\)\-]*$/.test(inputElem.value)) {
 				inputElem.value = defValue;
-				console.log('input default');
+				console.log('Default');
 			} else {
-				defValue = inputElem.value;
-				console.log('input normal');
+				if (!/^\+7((?<=\d)\(\d{0,3})?((?<=[\d\(])\)\d{0,3})?((?<=\)[\d\-]*)(\-\d{0,2})){0,2}$/.test(inputElem.value)) {
+					inputElem.value = inputElem.value.replace(/^(?:\+7)?\(?(\d{0,3})\)?(\d{0,3})\-?(\d{0,2})\-?(\d{0,2})$/, function(str, p1, p2, p3, p4) {
+						var res = '';
+
+						console.log(arguments);
+
+						if (p4 != '') {
+							res = '+7('+ p1 +')'+ p2 +'-'+ p3 +'-'+ p4;
+						} else if (p3 != '') {
+							res = '+7('+ p1 +')'+ p2 +'-'+ p3;
+						} else if (p2 != '') {
+							res = '+7('+ p1 +')'+ p2;
+						} else if (p1 != '') {
+							res = '+7('+ p1;
+						}
+
+						return res;
+					});
+
+					console.log('Replace');
+				}
+
+				if (!/^\+7((?<=\d)\(\d{0,3})?((?<=[\d\(])\)\d{0,3})?((?<=\)[\d\-]*)(\-\d{0,2})){0,2}$/.test(inputElem.value)) {
+					inputElem.value = defValue;
+					console.log('Default 2');
+				} else {
+					defValue = inputElem.value;
+					console.log('Not default');
+				}
+
 			}
+
+
 
 			//console.log('curs pos before', inputElem.selectionStart, inputElem.selectionEnd);
 
-			/*var cursPos = inputElem.selectionStart,
+/*
+			var cursPos = inputElem.selectionStart,
 			//rawValue = inpValue.replace(/(\+7|\D)+/g, ''),
-			replacedValue = inpValue.replace(/^(?:\+7)?\(?(\d{0,3})\)?(\d{0,3})\-?(\d{0,2})\-?(\d{0,2})$/, function(str, p1, p2, p3, p4) {
+			replacedValue = npValue.replace(/^(?:\+7)?\(?(\d{0,3})\)?(\d{0,3})\-?(\d{0,2})\-?(\d{0,2})$/, function(str, p1, p2, p3, p4) {
 				var repl = '';
 
 				console.log(arguments);
@@ -39,7 +70,7 @@ var Maskinput;
 				}
 
 				return repl;
-			});*/
+			});
 
 			//console.log(replacedValue, rawValue);
 			
@@ -50,7 +81,7 @@ var Maskinput;
 			if (/^\+7\(\d{0,3}\)\d{0,3}\-\d{0,2}\-\d{2}$/.test(inputElem.value)) {
 				//inputElem.selectionStart = inputElem.selectionEnd = cursPos;
 			}
-
+*/
 			
 
 			
