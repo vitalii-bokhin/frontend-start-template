967f3a7889f02242c46800e2c6df5818bdb0d35f
Lufter <lufter21@gmail.com>
2018-11-08 05:22:43 +0200

    form file improved

--- src/js/common.js
+++ src/js/common.js
@@ -6,225 +6,225 @@ document.addEventListener('DOMContentLoaded', function() {
 	(function initFun() {
 		if (window.innerWidth < 1200) {
 			var fsElem = document.querySelector('.first-screen');
-
+			
 			if (fsElem) {
 				fsElem.style.height = window.innerHeight +'px';
 			}
 		}
 		
 		FlexImg('.flex-img');
-
+		
 		CoverImg.reInit('body');
-
+		
 		Tab.reInit();
-
+		
 		window.addEventListener('winResized', initFun);
 	})();
-
+	
 	//init cover images
 	CoverImg.init();
-
+	
 	//init toggle button
 	Toggle.init('.js-toggle');
-
+	
 	Toggle.onChange = function(tgl, state) {
 		
 	}
-
+	
 	//ajax button
 	/*Ajax.init('.js-ajax');
-
+	
 	Ajax.success = function(response) {
 		// code...
 	}*/
-
+	
 	//popup init
 	Popup.init('.js-open-popup');
 	MediaPopup.init('.js-open-media-popup');
-
+	
 	//menu
 	if (window.innerWidth < 1000) {
 		Menu.init('.menu__item_has-children', '.menu__sub-menu');
 	}
-
+	
 	//mobile nav
 	MobNav.init({
 		openBtn: '.js-open-menu',
 		closeBtn: '.js-close-menu',
 		navId: 'header-mob-menu'
 	});
-
+	
 	//bubble
 	Bubble.init({
 		element: '.js-bubble'
 	});
-
+	
 	//accord
 	Accord.init('.accord__button');
-
+	
 	//more
 	More.init('.more__btn');
-
+	
 	//tab
 	Tab.init({
 		container: '.tab',
 		button: '.tab__button',
 		item: '.tab__item'
 	});
-
+	
 	//video
 	Video.init('.video__btn-play');
-
+	
 	//fullscreen scroll
 	FsScroll.init({
 		container: '.fsscroll',
 		screen: '.fsscroll__screen',
 		duration: 700
 	});
-
+	
 	//anchor
 	Anchor.init('.js-anchor', 700, 100);
-
+	
 	//diagram
 	var diagram = new Diagram({
 		canvasId: 'diagram',
 		charts: [
-		{
-			value: 37,
-			color: 'green',
-			width: 20,
-			numContId: 'diagram-num-1'
-		},
-		{
-			value: 45,
-			color: '#d0295e',
-			width: 10,
-			offset: 2,
-			numContId: 'diagram-num-2'
-		}
+			{
+				value: 37,
+				color: 'green',
+				width: 20,
+				numContId: 'diagram-num-1'
+			},
+			{
+				value: 45,
+				color: '#d0295e',
+				width: 10,
+				offset: 2,
+				numContId: 'diagram-num-2'
+			}
 		],
 		maxValue: 100
 	});
-
-
+	
+	
 	//diagram 2
 	var diagram_2 = new Diagram({
 		canvasId: 'diagram-2',
 		charts: [
-		{
-			value: 84,
-			color: '#fd8d40',
-			width: 30,
-			numContId: 'diagram-2-num-1'
-		},
-		{
-			value: 39,
-			color: '#0000ff',
-			width: 30,
-			offset: 2,
-			numContId: 'diagram-2-num-2'
-		}
+			{
+				value: 84,
+				color: '#fd8d40',
+				width: 30,
+				numContId: 'diagram-2-num-1'
+			},
+			{
+				value: 39,
+				color: '#0000ff',
+				width: 30,
+				offset: 2,
+				numContId: 'diagram-2-num-2'
+			}
 		],
 		maxValue: 100,
 		animate: true
 	});
-
+	
 	diagram_2.animate(2700);
-
+	
 	//diagram 2
 	var diagram_3 = new Diagram({
 		canvasId: 'diagram-3',
 		charts: [
-		{
-			value: 67,
-			color: '#fd8d40',
-			width: 15,
-			numContId: 'diagram-3-num-1'
-		},
-		{
-			value: 75,
-			color: '#d0295e',
-			width: 15,
-			offset: 2,
-			numContId: 'diagram-3-num-2'
-		},
-		{
-			value: 83,
-			color: 'green',
-			width: 15,
-			offset: 2,
-			numContId: 'diagram-3-num-3'
-		},
-		{
-			value: 91,
-			color: '#0000ff',
-			width: 15,
-			offset: 2,
-			numContId: 'diagram-3-num-4'
-		}
+			{
+				value: 67,
+				color: '#fd8d40',
+				width: 15,
+				numContId: 'diagram-3-num-1'
+			},
+			{
+				value: 75,
+				color: '#d0295e',
+				width: 15,
+				offset: 2,
+				numContId: 'diagram-3-num-2'
+			},
+			{
+				value: 83,
+				color: 'green',
+				width: 15,
+				offset: 2,
+				numContId: 'diagram-3-num-3'
+			},
+			{
+				value: 91,
+				color: '#0000ff',
+				width: 15,
+				offset: 2,
+				numContId: 'diagram-3-num-4'
+			}
 		],
 		maxValue: 100,
 		animate: true
 	});
-
+	
 	diagram_3.animate(4200);
-
+	
 	//numberspin
 	var numberspin = new Numberspin('.numberspin');
-
+	
 	numberspin.animate(4200);
-
+	
 	//share
 	Share.init('.js-share-btn');
-
+	
 	//timer
 	var timer = new Timer({
 		elemId: 'timer'
 	});
-
+	
 	timer.onStop = function() {
 		Popup.message('#message-popup', 'Timer Stopped');
 	}
-
+	
 	timer.start(50);
-
-
+	
+	
 	var timer2 = new Timer({
 		elemId: 'timer-2',
 		format: 'extended'
 	});
-
+	
 	timer2.onStop = function() {
 		Popup.message('#message-popup', 'Timer 2 Stopped');
 	}
-
+	
 	timer2.start(130);
-
-
+	
+	
 	var stopwatch = new Timer({
 		elemId: 'stopwatch', 
 		stopwatch: true
 	});
-
+	
 	stopwatch.onStop = function() {
 		Popup.message('#message-popup', 'Stopwatch Stopped');
 	}
-
+	
 	stopwatch.start(0);
-
-
+	
+	
 	var stopwatch2 = new Timer({
 		elemId: 'stopwatch-1', 
 		stopwatch: true,
 		format: 'extended'
 	});
-
+	
 	stopwatch2.onStop = function() {
 		Popup.message('#message-popup', 'Stopwatch Stopped');
 	}
-
+	
 	stopwatch2.start(0);
-
+	
 	//get content via Ajax
 	var getCont = new GetContentAjax({
 		eventBtn: '.js-get-content-ajax',
@@ -232,34 +232,40 @@ document.addEventListener('DOMContentLoaded', function() {
 		outputDiv: '#output-ajax',
 		sourceFile: '/get-content-ajax.php'
 	});
-
+	
 	getCont.output = function(response) {
 		var result = response.match(/\<div id\="source"\>([\s\S]*?)\<\/div\>/);
-
+		
 		return result[1];
 	}
-
-
+	
+	
 	//submit form
 	Form.init('.form');
-
+	
 	Form.onSubmit = function(form, callback) {
 		switch (form.id) {
 			case 'form-no-ajax':
 			case 'search-form':
 			return true;
-			break;
 
+			case 'custom-form-2':
+			case 'custom-form-3':
+			var files = CustomFile.files(form);
+			
+			console.log(files);
+			return false;
+			
 			default:
 			ajax({
 				url: form.action,
 				send: new FormData(form),
 				success: function(response) {
 					var response = JSON.parse(response);
-
+					
 					if (response.status == 'sent') {
 						Popup.message('#message-popup', 'Форма отправлена');
-
+						
 						callback({clearForm: true, unlockSubmitButton: true});
 					} else {
 						console.log(response);
@@ -269,7 +275,7 @@ document.addEventListener('DOMContentLoaded', function() {
 					console.log(response);
 				}
 			});
-
+			
 			return false;
 		}
 	}
@@ -300,16 +306,16 @@ $(document).ready(function(){
 	/*$('#slider').on('init', function() {
 		CoverImg.reInit('#slider');
 	});
-
+	
 	$('#slider').slick({
 		infinite: true,
 		slidesToShow: 1,
 		slidesToScroll: 1
 	});*/
-
+	
 	//scroll pane
 	//$('.scroll-pane').jScrollPane();
-
+	
 	//masked inputs
 	//$('input[data-type="tel"]').mask('+7(999)999-99-99');
 	$('input[data-type="tel"]').each(function() {
--- src/modules/form/file/file.js
+++ src/modules/form/file/file.js
@@ -2,90 +2,99 @@ var CustomFile;
 
 (function() {
 	"use strict";
-
+	
 	//custom file
 	CustomFile = {
 		input: null,
 		filesObj: {},
 		filesArrayObj: {},
-
+		
 		loadPreview: function(file, fileItem) {
 			var reader = new FileReader(),
 			previewDiv = document.createElement('div');
-
+			
 			previewDiv.className = 'custom-file__preview';
-
+			
 			fileItem.insertBefore(previewDiv, fileItem.firstChild);
-
+			
 			reader.onload = function(e) {
 				setTimeout(function() {
 					var imgDiv = document.createElement('div');
-
+					
 					imgDiv.innerHTML = '<img src="'+ e.target.result +'">';
-
+					
 					previewDiv.appendChild(imgDiv);
 				}, 121);
 			}
-
+			
 			reader.readAsDataURL(file);
 		},
-
+		
 		changeInput: function(elem) {
 			var fileItems = elem.closest('.custom-file').querySelector('.custom-file__items');
-
-			fileItems.innerHTML = '';
+			
+			if (elem.getAttribute('data-action') == 'clear') {
+				fileItems.innerHTML = '';
+			}
 
 			for (var i = 0; i < elem.files.length; i++) {
-				var file = elem.files[i],
-				fileItem = document.createElement('div');
+				var file = elem.files[i];
 
+				if (this.filesObj[elem.id] && this.filesObj[elem.id][file.name] != undefined) continue;
+
+				var fileItem = document.createElement('div');
+				
 				fileItem.className = 'custom-file__item';
 				fileItem.innerHTML = '<div class="custom-file__name">'+ file.name +'</div><button type="button" class="custom-file__del-btn" data-ind="'+ file.name +'"></button>';
-
+				
 				fileItems.appendChild(fileItem);
-
+				
 				if (file.type.match(/image.*/)) {
 					this.loadPreview(file, fileItem);
 				}
 			}
-
+			
 			this.setFilesObj(elem.files);
 		},
-
+		
 		setFilesObj: function(filesList, objKey) {
 			var inputElem = this.input;
-
+			
 			if (!inputElem.id.length) {
 				inputElem.id = 'custom-file-input-'+ new Date().valueOf();
 			}
-
+			
 			if (filesList) {
-				this.filesObj[inputElem.id] = {};
-
+				if (inputElem.getAttribute('data-action') == 'clear') {
+					this.filesObj[inputElem.id] = {};
+				} else {
+					this.filesObj[inputElem.id] = this.filesObj[inputElem.id] || {};
+				}
+				
 				for (var i = 0; i < filesList.length; i++) {
 					this.filesObj[inputElem.id][filesList[i].name] = filesList[i];
 				}
 			} else {
 				delete this.filesObj[inputElem.id][objKey];
 			}
-
+			
 			this.filesArrayObj[inputElem.id] = [];
-
+			
 			for (var key in this.filesObj[inputElem.id]) {
 				this.filesArrayObj[inputElem.id].push(this.filesObj[inputElem.id][key]);
 			}
-
+			
 			ValidateForm.file(inputElem, this.filesArrayObj[inputElem.id]);
 		},
-
+		
 		inputFiles: function(inputElem) {
 			return this.filesArrayObj[inputElem.id] || [];
 		},
-
+		
 		files: function(formElem) {
 			var inputFileElements = formElem.querySelectorAll('.custom-file__input'),
 			filesArr = [];
-
+			
 			if (inputFileElements.length == 1) {
 				filesArr = this.filesArrayObj[inputFileElements[0].id];
 			} else {
@@ -95,44 +104,40 @@ var CustomFile;
 					}
 				}
 			}
-
+			
 			return filesArr;
 		},
-
+		
 		init: function() {
 			document.addEventListener('change', (e) => {
 				var elem = e.target.closest('input[type="file"]');
-
-				if (!elem) {
-					return;
-				}
-
+				
+				if (!elem) return;
+				
 				this.input = elem;
-
+				
 				this.changeInput(elem);
 			});
-
+			
 			document.addEventListener('click', (e) => {
 				var delBtnElem = e.target.closest('.custom-file__del-btn'),
 				inputElem = e.target.closest('input[type="file"]');
-
+				
 				if (inputElem) {
 					inputElem.value = null;
 				}
-
-				if (!delBtnElem) {
-					return;
-				}
-
+				
+				if (!delBtnElem) return;
+				
 				this.input = delBtnElem.closest('.custom-file').querySelector('.custom-file__input');
-
+				
 				delBtnElem.closest('.custom-file__items').removeChild(delBtnElem.closest('.custom-file__item'));
-
+				
 				this.setFilesObj(false, delBtnElem.getAttribute('data-ind'));
 			});
 		}
 	};
-
+	
 	//init scripts
 	document.addEventListener('DOMContentLoaded', function() {
 		CustomFile.init();
