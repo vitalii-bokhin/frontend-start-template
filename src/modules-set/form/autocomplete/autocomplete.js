6a45b565cec02bba766fdd93be2052c26617b942
Lufter <lufter21@gmail.com>
2018-12-01 14:27:54 +0200

    fix @media

--- src/modules-set/form/autocomplete/autocomplete.js
+++ src/modules-set/form/autocomplete/autocomplete.js
@@ -5,11 +5,13 @@
 	
 	AutoComplete = {
 		field: null,
-		srcData: null,
-
+		input: null,
+		valuesData: null,
+		inputValue: '',
+		
 		open: function () {
 			this.field.classList.add('autocomplete_opened');
-
+			
 			var opionsElem = this.field.querySelector('.autocomplete__options');
 			
 			opionsElem.style.height = opionsElem.scrollHeight +'px';
@@ -22,24 +24,26 @@
 				}
 			}, 550);
 		},
-
+		
 		close: function () {
 			this.field.classList.remove('autocomplete_opened');
-
+			
 			var opionsElem = this.field.querySelector('.autocomplete__options');
-
+			
 			opionsElem.classList.remove('ovfauto');
-
+			
 			opionsElem.style.height = 0;
 		},
 		
-		complete: function (inpElem) {
+		getValues: function () {
 			var optionsElem = this.field.querySelector('.autocomplete__options');
 			
-			if (inpElem.value.length) {
-				var reg = new RegExp('^'+ inpElem.value, 'i'),
-				data = JSON.parse(this.srcData),
+			if (this.input.value.length) {
+				var reg = new RegExp('^'+ this.input.value, 'i'),
+				data = JSON.parse(this.valuesData),
 				values = '';
+
+				this.inputValue = this.input.value;
 				
 				for (var i = 0; i < data.length; i++) {
 					var dataVal = data[i];
@@ -50,19 +54,25 @@
 				}
 				
 				optionsElem.innerHTML = values;
-
+				
 				if (values != '') {
 					this.open();
 				} else {
 					this.close();
 				}
-
+				
 				values = '';
 			} else {
 				optionsElem.innerHTML = '';
 				
 				this.close();
 			}
+		},
+
+		selectVal: function (itemElem) {
+			var valueElem = itemElem.querySelector('.autocomplete__val');
+
+			this.input.value = valueElem.innerHTML;
 		},
 		
 		keybinding: function (e) {
@@ -72,89 +82,64 @@
 			
 			e.preventDefault();
 			
-			var options = this.field.querySelector('.autocomplete__options'),
-			hoverItem = options.querySelector('li.hover');
+			var optionsElem = this.field.querySelector('.autocomplete__options'),
+			hoverItem = optionsElem.querySelector('li.hover');
 			
 			switch (key) {
 				case 40:
 				if (hoverItem) {
-					var nextItem = (function (item) {
-						var elem = item.nextElementSibling;
-						
-						while (elem) {
-							if (!elem) break;
-							
-							if (!elem.elementIsHidden()) {
-								return elem;
-							} else {
-								elem = elem.nextElementSibling;
-							}
-						}
-					})(hoverItem);
+					var nextItem = hoverItem.nextElementSibling;
 					
 					if (nextItem) {
 						hoverItem.classList.remove('hover');
 						nextItem.classList.add('hover');
 						
-						options.scrollTop = options.scrollTop + (nextItem.getBoundingClientRect().top - options.getBoundingClientRect().top);
+						optionsElem.scrollTop = optionsElem.scrollTop + (nextItem.getBoundingClientRect().top - optionsElem.getBoundingClientRect().top);
+
+						this.selectVal(nextItem);
 					}
 				} else {
-					var elem = options.firstElementChild;
+					var nextItem = optionsElem.firstElementChild;
 					
-					while (elem) {
-						if (!elem) break;
-						
-						if (!elem.elementIsHidden()) {
-							elem.classList.add('hover');
-							break;
-						} else {
-							elem = elem.nextElementSibling;
-						}
+					if (nextItem) {
+						nextItem.classList.add('hover');
+
+						this.selectVal(nextItem);
 					}
 				}
 				break;
 				
 				case 38:
 				if (hoverItem) {
-					var nextItem = (function (item) {
-						var elem = item.previousElementSibling;
-						
-						while (elem) {
-							if (!elem) break;
-							
-							if (!elem.elementIsHidden()) {
-								return elem;
-							} else {
-								elem = elem.previousElementSibling;
-							}
-						}
-					})(hoverItem);
+					var nextItem = hoverItem.previousElementSibling;
 					
 					if (nextItem) {
 						hoverItem.classList.remove('hover');
 						nextItem.classList.add('hover');
 						
-						options.scrollTop = options.scrollTop + (nextItem.getBoundingClientRect().top - options.getBoundingClientRect().top);
+						optionsElem.scrollTop = optionsElem.scrollTop + (nextItem.getBoundingClientRect().top - optionsElem.getBoundingClientRect().top);
+
+						this.selectVal(nextItem);
 					}
 				} else {
-					var elem = options.lastElementChild;
+					var nextItem = optionsElem.lastElementChild;
 					
-					while (elem) {
-						if (!elem) break;
-						
-						if (!elem.elementIsHidden()) {
-							elem.classList.add('hover');
-							options.scrollTop = 9999;
-							break;
-						} else {
-							elem = elem.previousElementSibling;
-						}
+					if (nextItem) {
+						nextItem.classList.add('hover');
+
+						optionsElem.scrollTop = 9999;
+
+						this.selectVal(nextItem);
 					}
 				}
 				break;
 				
 				case 13:
-				this.selectVal(hoverItem.querySelector('.custom-select__val'));
+				if (hoverItem) {
+					this.selectVal(hoverItem);
+
+					this.input.blur();
+				}
 			}
 		},
 		
@@ -166,43 +151,32 @@
 				if (!elem) return;
 				
 				this.field = elem.closest('.autocomplete');
-
+				this.input = elem;
+				
 				if (this.field.querySelector('.autocomplete__val')) {
 					this.open();
 				}
 			}, true);
-
+			
 			// blur event
 			document.addEventListener('blur', (e) => {
-				var elem = e.target.closest('.autocomplete__input');
-				
-				if (!elem) return;
-				
-				this.field = elem.closest('.autocomplete');
-
-				this.close();
+				if (e.target.closest('.autocomplete__input')) {
+					this.close();
+				}
 			}, true);
 			
 			// input event
 			document.addEventListener('input', (e) => {
-				var elem = e.target.closest('.autocomplete__input');
-				
-				if (!elem) return;
-				
-				this.field = elem.closest('.autocomplete');
-				
-				this.complete(elem);
+				if (e.target.closest('.autocomplete__input')) {
+					this.getValues();
+				}
 			});
 			
 			// keyboard events
 			document.addEventListener('keydown', (e) => {
-				var elem = e.target.closest('.autocomplete_opened');
-				
-				if (!elem) return;
-				
-				this.field = elem.closest('.autocomplete');
-				
-				this.keybinding(e);
+				if (e.target.closest('.autocomplete_opened')) {
+					this.keybinding(e);
+				}
 			});
 		}
 	};
