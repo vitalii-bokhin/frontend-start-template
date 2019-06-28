ccf56d9777bc25bdcae0e0d9f1d822c1e05edcb8
Lufter <lufter21@gmail.com>
2019-06-28 22:24:55 +0300

    com

--- src/modules/share/share.js
+++ src/modules/share/share.js
@@ -4,47 +4,48 @@ Share.init(Str button class);
 
 ; var Share;
 
-;(function() {
+; (function () {
 	'use strict';
-	
-	var encodedHref = encodeURIComponent(window.location.href);
 
 	Share = {
-		network: function(elem) {
-			var net = elem.getAttribute('data-network');
+		network: function (elem) {
+			let net = elem.getAttribute('data-network');
 
 			if (!net) {
 				return;
 			}
 
-			var url;
+			let encodedHref = (elem.hasAttribute('data-share-url')) ? encodeURIComponent(elem.getAttribute('data-share-url')) : encodeURIComponent(window.location.href),
+				encodedImageUrl = (elem.hasAttribute('data-share-img')) ? encodeURIComponent(elem.getAttribute('data-share-img')) : null,
+				title = (elem.hasAttribute('data-share-tit')) ? encodeURIComponent(elem.getAttribute('data-share-tit')) : null,
+				url;
 
 			switch (net) {
 				case 'vk':
-				url = 'http://vkontakte.ru/share.php?url='+ encodedHref;
-				break;
+					url = 'https://vkontakte.ru/share.php?url=' + encodedHref + ((encodedImageUrl) ? '&image=' + encodedImageUrl : '') + ((title) ? '&title=' + title : '');
+					break;
 
 				case 'fb':
-				url = 'http://www.facebook.com/sharer.php?u='+ encodedHref;
-				break;
+					url = 'https://www.facebook.com/sharer.php?u=' + encodedHref;
+					break;
 
 				case 'tw':
-				url = 'http://twitter.com/share?url='+ encodedHref;
-				break;
+					url = 'http://twitter.com/share?url=' + encodedHref;
+					break;
 
 				case 'ok':
-				url = 'https://connect.ok.ru/dk?st.cmd=WidgetSharePreview&st.shareUrl='+ encodedHref;
-				break;
+					url = 'https://connect.ok.ru/offer?url=' + encodedHref + ((encodedImageUrl) ? '&imageUrl=' + encodedImageUrl : '') + ((title) ? '&title=' + title : '');
+					break;
 			}
 
 			this.popup(url);
 		},
 
-		popup: function(url) {
+		popup: function (url) {
 			window.open(url, '', 'toolbar=0,status=0,width=626,height=436');
 		},
 
-		init: function(elementStr) {
+		init: function (elementStr) {
 			document.addEventListener('click', (e) => {
 				var elem = e.target.closest(elementStr);
 
