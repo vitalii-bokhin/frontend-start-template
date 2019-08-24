18dd77f2bf6be25c9772ac0bdd3ed7de15f1f270
Lufter <lufter21@gmail.com>
2019-08-24 16:13:34 +0300

    com

--- src/modules/toggle/toggle.js
+++ src/modules/toggle/toggle.js
@@ -24,15 +24,6 @@ Toggle.onChange = function(toggleElem, state) {
 				for (var i = 0; i < targetElements.length; i++) {
 					targetElements[i].classList.add(this.toggledClass);
 				}
-				
-				//dependence elements
-				if (toggleElem.hasAttribute('data-dependence-target-elements')) {
-					var dependenceTargetElements = document.querySelectorAll(toggleElem.getAttribute('data-dependence-target-elements'));
-					
-					for (var i = 0; i < dependenceTargetElements.length; i++) {
-						dependenceTargetElements[i].classList.remove(this.toggledClass);
-					}
-				}
 			} else {
 				for (var i = 0; i < targetElements.length; i++) {
 					targetElements[i].classList.remove(this.toggledClass);
@@ -71,6 +62,17 @@ Toggle.onChange = function(toggleElem, state) {
 			//call onChange
 			if (this.onChange) {
 				this.onChange(toggleElem, state);
+			}
+
+			if (!state) return;
+
+			//dependence elements
+			if (toggleElem.hasAttribute('data-dependence-target-elements')) {
+				const dependenceTargetElements = document.querySelectorAll(toggleElem.getAttribute('data-dependence-target-elements'));
+				
+				for (let i = 0; i < dependenceTargetElements.length; i++) {
+					dependenceTargetElements[i].classList.remove(this.toggledClass);
+				}
 			}
 		},
 		
